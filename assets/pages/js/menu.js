e9ecc28b7734de15542ebcede8e56cea1ea2bd3e
laguna99999 <lookatme99999@yahoo.com>
2020-10-07 23:59:03 +0200

    Menu page done

--- assets/pages/js/menu.js
+++ assets/pages/js/menu.js
@@ -1,7 +1,34 @@
-$('.items .item').click(function(){
-  if($(this).find('.item-details').hasClass('hide')){
-    $(this).find('.item-details').removeClass('hide')
-  }else{
-    $(this).find('.item-details').addClass('hide')
+let modal = document.querySelector('.modal');
+
+document.querySelectorAll('.menu-item').forEach(item => {
+  item.addEventListener('click', function(){
+    this.querySelector('.menu-item-details').classList.toggle('hide');
+  })
+})
+
+document.querySelectorAll('.menu-item-detail').forEach(item => {
+  item.onclick = () => {
+    modal.style.display = 'flex';
   }
 })
+
+document.querySelectorAll('.accordion').forEach(item => {
+  item.addEventListener('click', function(){
+    this.classList.toggle('active');
+    let panel = this.nextElementSibling;
+    if(panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    }else{
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+  })
+})
+
+let modal_close = () => {
+  modal.style.display = 'none';
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
